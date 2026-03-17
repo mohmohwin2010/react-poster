03bddb77bf8c1f08a0ab322bc976b6fe9c0da3ef
mohmohwin2010 <mohmohwin2010@gmail.com>
2026-03-17 23:13:07 +0630

    feat: data fetching via loader(), submitting data with action()

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,22 @@ import React from 'react'
 import  ReactDom  from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import Posts from './routes/Posts.jsx'
+import Posts, {loader as postsLoader} from './routes/Posts.jsx'
 import './index.css'
-import NewPost from './routes/NewPost.jsx';
+import NewPost, {action as newPostAction} from './routes/NewPost.jsx';
 import RootLayout from './routes/RootLayout.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-     element: <RootLayout />, children: [
-    { path: "/", element: <Posts />, children: [
-      { path: "/create-post", element: <NewPost /> }
-    ]},
+    element: <RootLayout />, 
+    children: [
+    { 
+      path: "/", 
+      element: <Posts />, 
+      loader: postsLoader,
+      children: [{ path: "/create-post", element: <NewPost />, action: newPostAction }]
+  },
   
   ]},
   
